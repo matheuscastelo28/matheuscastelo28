7e7c3006e1e9b715b7895fa1c664e6586cd86a87
matheus castelo <castelomatheus9@gmail.com>
2025-08-08 17:34:20 +0000

    matheus e fael estudando

---
+++ teste.ferias.js
@@ -0,0 +1,9 @@
+let nome = "Matheus castelo";
+console.log(nome);
+
+const horario = 8;
+if (horario<=5){
+    console.log("VOCE  CHEGOU ATRASADO");
+} else {
+    console.log("VOCE ESTA NO HORARIO");
+}
